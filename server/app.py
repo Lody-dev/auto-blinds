1ff8d398880d01817bdb07fdff2ad34aeb28a034
Lody-dev <lodyiaremko@proton.me>
2025-10-19 15:28:54 +0000

    Handeling conversion from raw numbers to % in slider

--- server/app.py
+++ server/app.py
@@ -5,21 +5,37 @@ import subprocess
 
 app = Flask(__name__)
 
-slider_position = 50
-
 ESP32_IP = "192.168.18.131"  
+
+def curl_get(path):
+    result = subprocess.run(
+        ["curl", "-s", f"http://{ESP32_IP}/{path}"],
+        capture_output=True,
+        text=True
+    )
+    try:
+        result = int(result.stdout.strip())
+    except:
+        result = 0
+    return result
+
+slider_position = curl_get("get_current_pos")
+max_pos = curl_get("get_max_pos")
+set_up_status = curl_get("get_set_up_status")
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     global slider_position
+    slider_position = int(request.form.get("value", slider_position))
+    sent_goto = (slider_position * max_pos) / 100;
     if request.method == "POST":
-        slider_position = int(request.form.get("value", slider_position))
-        cmd = ["curl", f"http://{ESP32_IP}/goto?pos={slider_position}"]
+        cmd = ["curl", f"http://{ESP32_IP}/goto?pos={sent_goto}"]
         try:
             result = subprocess.run(cmd, capture_output=True, text=True, check=True)
             print("Curl output:", result.stdout)
         except subprocess.CalledProcessError as e:
             print("Curl failed:", e)
     return render_template("index.html", slider_position=slider_position)
+
 
 app.run(host="0.0.0.0", port=5000)
